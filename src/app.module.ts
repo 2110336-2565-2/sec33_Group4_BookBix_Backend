ad95b9d4fa1bf938b799624b76c3f091f5268f8b
S1xZ <punya.gnwdn@gmail.com>
2023-02-02 19:57:57 +0700

    Configure dotenv template stuff and Install Mongoose
    + dotenv
    + Mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { config } from 'dotenv';
+config();
 
 @Module({
   imports: [],
