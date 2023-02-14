261fb3d5eeb166e47d225b3f13d309394861ba4c
chanakorn aramsak <solo20113@gmail.com>
2023-02-14 20:06:31 +0000

    add email module to route the SMTP

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { CustomersModule } from './customers/customers.module';
-
-
+import { ConfigModule } from '@nestjs/config';
+import { EmailModule } from "./customers/services/email.module";
 @Module({
-  imports: [AuthModule, CustomersModule, MongooseModule.forRoot(
-    //database url string
-    process.env.MONGODB_DBURL
-    ) ],
+  imports: [
+    AuthModule,
+    CustomersModule,
+    EmailModule,
+    MongooseModule.forRoot(
+      //database url string
+      process.env.MONGODB_DBURL,
+    ),
+    ConfigModule.forRoot({
+      isGlobal: true, // no need to import into other modules
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
