56968275f58d1e243defcb637b671e7e9f998372
chanakorn aramsak <solo20113@gmail.com>
2023-02-15 05:16:17 +0000

    create service,controller for send email

--- src/customers/customers.controller.ts
+++ src/customers/customers.controller.ts
@@ -7,12 +7,14 @@ import {
   Request,
   Res,
   UseGuards,
+  Param,
 } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 import { AuthenticatedGuard } from 'src/auth/authenticated.guard';
 import { LocalAuthGuard } from 'src/auth/local.auth.guard';
 import { CustomersService } from './customers.service';
 import DeviceDetector = require('device-detector-js');
+import { EmailService } from './services/email.service';
 
 const deviceDetector = new DeviceDetector();
 function getDevice(headers: { 'user-agent': string }): string {
@@ -34,7 +36,10 @@ function getDevice(headers: { 'user-agent': string }): string {
 
 @Controller('customers')
 export class CustomersController {
-  constructor(private readonly customerService: CustomersService) {}
+  constructor(
+    private readonly emailService: EmailService,
+    private readonly customerService: CustomersService,
+  ) {}
 
   @Get('/register')
   async renderRegisterPage(@Res() res) {
@@ -71,6 +76,11 @@ export class CustomersController {
     };
   }
 
+  @Get('/login')
+  async renderLoginPage(@Res() res) {
+    return res.render('login');
+  }
+
   @UseGuards(LocalAuthGuard)
   @Post('/login')
   login(@Request() req): any {
@@ -100,5 +110,70 @@ export class CustomersController {
   logout(@Request() req): any {
     req.session.destroy();
     return { msg: 'The user session has ended' };
+  }
+
+  @Get('/resetpassword')
+  async renderResetPasswordPage(@Res() res) {
+    return res.render('resetpassword');
+  }
+
+  @Post('/resetpassword')
+  async resetPassword(@Body('email') email: string) {
+    const customer = await this.customerService.getCustomerByEmail(email);
+    if (!customer) {
+      // customer not found
+      return { msg: 'No customer found with that email address' };
+    }
+
+    const token = await this.customerService.generatePasswordResetToken(
+      customer.id,
+    );
+    if (!token) {
+      // error generating token
+      return { msg: 'Error generating password reset token' };
+    }
+
+    await this.customerService.sendPasswordResetEmail(customer);
+
+    return { msg: 'Password reset email sent' };
+  }
+
+  @Get('/resetpassword/:token')
+  async renderNewPasswordPage(@Param('token') token: string, @Res() res) {
+    const isValidToken = await this.customerService.validatePasswordResetToken(
+      token,
+    );
+    if (!isValidToken) {
+      // invalid token
+      return res.status(400).send({ msg: 'Invalid password reset token' });
+    }
+
+    return res.render('newpassword', { token });
+  }
+
+  @Put('/resetpassword/:token')
+  async setNewPassword(
+    @Param('token') token: string,
+    @Body('password') password: string,
+    @Body('confirmPassword') confirmPassword: string,
+  ) {
+    if (password !== confirmPassword) {
+      // password and confirm password do not match
+      return { msg: 'Password and confirm password do not match' };
+    }
+
+    const isValidToken = await this.customerService.validatePasswordResetToken(
+      token,
+    );
+    if (!isValidToken) {
+      // invalid token
+      return { msg: 'Invalid password reset token' };
+    }
+
+    const saltOrRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltOrRounds);
+    await this.customerService.updatePasswordUsingToken(token, hashedPassword);
+
+    return { msg: 'Password updated successfully' };
   }
 }
--- src/customers/customers.module.ts
+++ src/customers/customers.module.ts
@@ -2,14 +2,15 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CustomersController } from './customers.controller';
 import { CustomersService } from './customers.service';
-import { CustomerSchema } from './customers.model';
+import { CustomerSchema } from "./customers.model";
+import { EmailService } from './services/email.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: 'customers', schema: CustomerSchema }]),
   ],
   controllers: [CustomersController],
-  providers: [CustomersService],
-  exports: [CustomersService],
+  providers: [CustomersService,EmailService],
+  exports: [CustomersService]
 })
 export class CustomersModule {}
