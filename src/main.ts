6742e57bba46af9b9deb5f9b8dfa806a1449b6b0
Punya Gunawardana <punya.gnwdn@gmail.com>
2023-02-23 15:37:10 +0700

    Fix cors again

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors:  
   {
     origin: 'http://localhost:3000',
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
   }});
 
   app.use(
