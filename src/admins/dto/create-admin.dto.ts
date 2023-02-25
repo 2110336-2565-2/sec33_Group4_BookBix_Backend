35a28a68f753b08e83ebe4fc902abc6e3768272f
chanakorn aramsak <solo20113@gmail.com>
2023-02-25 05:29:09 +0000

    add admin dto and entity

--- src/admins/dto/create-admin.dto.ts
+++ src/admins/dto/create-admin.dto.ts
@@ -1,1 +1,14 @@
-export class CreateAdminDto {}
+import { IsNotEmpty, IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
+export class CreateAdminDto {
+  @IsNotEmpty()
+  username: string;
+
+  @IsNotEmpty()
+  password: string;
+
+  @IsNotEmpty()
+  email: string;
+
+  date_created: Date;
+}
