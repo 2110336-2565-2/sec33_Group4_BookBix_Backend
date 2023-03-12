e2755767b6b9575a77237f669b421c045feb88e2
S1xZ <punya.gnwdn@gmail.com>
2023-03-12 20:05:43 +0700

    WIP

--- src/bookings/bookings.module.ts
+++ src/bookings/bookings.module.ts
@@ -4,11 +4,13 @@ import { CustomersModule } from 'src/customers/customers.module';
 import { BookingsController } from './bookings.controller';
 import { BookingSchema } from './bookings.model';
 import { BookingsService } from './bookings.service';
+import { ProvidersModule } from 'src/providers/providers.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: 'bookings', schema: BookingSchema }]),
     CustomersModule,
+    ProvidersModule
   ],
   controllers: [BookingsController],
   providers: [BookingsService],
