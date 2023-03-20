a8fd1cca922a93f177cd8627cbe441c0db5cebe7
chanakorn aramsak <solo20113@gmail.com>
2023-03-20 11:14:16 +0700

    add require parameter into checkout session

--- src/payment/stripe/stripe.service.ts
+++ src/payment/stripe/stripe.service.ts
@@ -34,28 +34,25 @@ export class StripeService {
     return accountLink.url;
   }
 
-  async createPaymentIntent(amount: number, accountId: string): Promise<Stripe.PaymentIntent> {
-    const paymentIntent = await this.stripe.paymentIntents.create({
-      amount: amount,
-      currency: 'thb',
-      application_fee_amount: Math.floor(amount * 0.1), // Collect a 10% platform fee on the payment
-      transfer_data: {
-        destination: accountId,
-      },
-    });
-    return paymentIntent;
-  }
   
-  async createCheckoutSession(priceId: string): Promise<Stripe.Checkout.Session> {
+  async createCheckoutSession(priceId: string, connectedAccountId:string, hour:number): Promise<Stripe.Checkout.Session> {
+    
+    const price = await this.stripe.prices.retrieve(priceId);
+    const applicationFeeAmount = Math.round(price.unit_amount * 0.1); // calculate 10% of the total price
+
     const session = await this.stripe.checkout.sessions.create({
-      line_items: [{ price: priceId, quantity: 1 }],
       mode: 'payment',
+      line_items: [{price: priceId, quantity: hour}],
+      payment_intent_data: {
+        application_fee_amount: applicationFeeAmount,
+        transfer_data: {destination: connectedAccountId},
+      },
       success_url: 'http://localhost:3000/me/bookings',
       cancel_url: 'http://localhost:3000/me/bookings',
     });
-  
     return session;
-  }
+}
+
   
   async capturePayment(paymentIntentId: string): Promise<Stripe.PaymentIntent> {
     const paymentIntent = await this.stripe.paymentIntents.capture(paymentIntentId);
@@ -86,54 +83,14 @@ export class StripeService {
 
   
 
-  async getProduct(productId: string): Promise<Stripe.Product> {
-    const product = await this.stripe.products.retrieve(productId);
-    return product;
-  }
+  // async constructWebhookEvent(payload: , signature: string | string[]): Promise<Stripe.Event> {
+  //   const event = this.stripe.webhooks.constructEvent(
+  //     payload,
+  //     signature,
+  //     process.env.STRIPE_ENDPOINT_SECRET
+  //     );
 
-  async updateProduct(productId: string, metadata: Stripe.MetadataParam): Promise<Stripe.Product> {
-    const product = await this.stripe.products.update(productId, { metadata });
-    return product;
-  }
-
-  async listProducts(limit: number): Promise<Stripe.ApiList<Stripe.Product>> {
-    const products = await this.stripe.products.list({
-      limit,
-    });
-    return products;
-  }
-
-  async deleteProduct(productId: string): Promise<Stripe.DeletedProduct> {
-    const deletedProduct = await this.stripe.products.del(productId);
-    return deletedProduct;
-  }
-
-  
-  async createPrice(amount: number, currency: string, productId: string): Promise<Stripe.Price> {
-    const price = await this.stripe.prices.create({
-      unit_amount: amount,
-      currency,
-      product: productId,
-    });
-    return price;
-  }
-
-  async getPrice(priceId: string): Promise<Stripe.Price> {
-    const price = await this.stripe.prices.retrieve(priceId);
-    return price;
-  }
-
-  async updatePrice(priceId: string, metadata: Stripe.MetadataParam): Promise<Stripe.Price> {
-    const price = await this.stripe.prices.update(priceId, { metadata });
-    return price;
-  }
-
-  async listPrices(limit: number): Promise<Stripe.ApiList<Stripe.Price>> {
-    const prices = await this.stripe.prices.list({
-      limit,
-    });
-    return prices;
-  }
-
+  //   return event;
+  // }
 
 }
