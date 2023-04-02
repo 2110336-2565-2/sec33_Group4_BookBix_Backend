9a6436e8a2435ee8541a15a95105b71e8e310acc
NatananPh <nut14586@gmail.com>
2023-04-02 17:03:31 +0700

    ✨Add GET API for getting the locations from provider id

--- src/providers/providers.module.ts
+++ src/providers/providers.module.ts
@@ -3,10 +3,12 @@ import { ProvidersService } from './providers.service';
 import { ProvidersController } from './providers.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProviderSchema } from './entities/provider.entity';
+import { LocationSchema } from 'src/locations/entity/locations.entity';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: 'providers', schema: ProviderSchema }]),
+    MongooseModule.forFeature([{ name: 'locations', schema: LocationSchema }]),
   ],
   controllers: [ProvidersController],
   providers: [ProvidersService],
--- src/providers/providers.service.ts
+++ src/providers/providers.service.ts
@@ -4,15 +4,15 @@ import { Model } from 'mongoose';
 import { CreateProviderDto } from './dto/create-provider.dto';
 import { UpdateProviderDto } from './dto/update-provider.dto';
 import { Provider } from './entities/provider.entity';
+import { Location } from 'src/locations/entity/locations.entity';
 
 @Injectable()
 export class ProvidersService {
-
   constructor(
     @InjectModel('providers') private readonly providerModel: Model<Provider>,
+    @InjectModel('locations') private readonly locationModel: Model<Location>,
   ) {}
-  
-  
+
   async insertNewProvider(createProviderDto: CreateProviderDto) {
     const newProvider = new this.providerModel(createProviderDto);
     await newProvider.save();
@@ -40,19 +40,40 @@ export class ProvidersService {
     return provider;
   }
 
-  async updateStripeAccountId(providerId: string, accountId: string): Promise<Provider> {
+  async updateStripeAccountId(
+    providerId: string,
+    accountId: string,
+  ): Promise<Provider> {
     const filter = { _id: providerId };
     const update = { $set: { stripe_account_id: accountId } };
     const options = { new: true };
-    const provider = await this.providerModel.findOneAndUpdate(filter, update, options);
+    const provider = await this.providerModel.findOneAndUpdate(
+      filter,
+      update,
+      options,
+    );
     return provider;
   }
   async getStripeAccountId(providerId: string): Promise<string> {
     const provider = await this.providerModel.findById(providerId);
     return provider.stripe_account_id;
   }
-  async getProviderEmailByStripeAccountId(stripeAccountId: string): Promise<string> {
-    const provider = await this.providerModel.findOne({ stripe_account_id: stripeAccountId });
+  async getProviderEmailByStripeAccountId(
+    stripeAccountId: string,
+  ): Promise<string> {
+    const provider = await this.providerModel.findOne({
+      stripe_account_id: stripeAccountId,
+    });
     return provider.email;
+  }
+  async getLocationsByProviderId(providerId: string) {
+    const provider = await this.providerModel.findById(providerId);
+    console.log(provider.locations);
+    let result = [];
+    for (let i = 0; i < provider.locations.length; i++) {
+      let location = await this.locationModel.findById(provider.locations[i]);
+      result.push(location);
+    }
+    return result;
   }
 }
