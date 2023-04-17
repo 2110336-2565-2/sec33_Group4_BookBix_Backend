62b83e4baad620e0fb1bb8a50fb0ae63dee03090
NatananPh <nut14586@gmail.com>
2023-04-17 12:23:44 +0700

    📚 Add Swagger documentation for Providers endpoint

--- src/providers/providers.controller.ts
+++ src/providers/providers.controller.ts
@@ -10,11 +10,19 @@ import {
 import { ProvidersService } from './providers.service';
 import { CreateProviderDto } from './dto/create-provider.dto';
 import { UpdateProviderDto } from './dto/update-provider.dto';
-
+import {
+  ApiTags,
+  ApiOperation,
+  ApiResponse,
+  ApiBody,
+  ApiQuery,
+} from '@nestjs/swagger'; // Import Swagger decorators
+@ApiTags('Providers') // Add tags for the API group
 @Controller('providers')
 export class ProvidersController {
   constructor(private readonly providersService: ProvidersService) {}
   @Get('locations/:providerId')
+  @ApiOperation({ summary: 'Get locations by provider id' })
   async getLocationsByProvideId(@Param('providerId') providerId: string) {
     return this.providersService.getLocationsByProviderId(providerId);
   }
